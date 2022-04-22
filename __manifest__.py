5b634021c62ec1002c07fe86ce4d0de56b64e6d9
Shamsul Arifin <arifinbd6@gmail.com>
2022-04-22 21:28:58 +0600

    Inventory Consolidated Report
    Inventory Consolidated Report moule for odoo 10.0

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Shamsul Arifin, Crystal Technology Bangladesh Ltd.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "ICR (Experimental)",
+    "version": "1.0.0.1",
+    "author": "Crystal Technology Bangladesh",
+    "category": "Reporting",
+    'summary': 'Inventory Consolidated Report.'
+               'User can view consolidated report of inventory',
+    'company': 'Crystal Technology Bangladesh Ltd.',
+    'website': 'www.ctechbd.com',
+    "description": """User can view consolidated report of inventory""",
+    "depends": ["product", "stock"],
+    "data": ["wizard/icr_report_stock_wizard.xml"],
+
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
