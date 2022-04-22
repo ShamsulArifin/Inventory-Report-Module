6b66101399bf17dd3fd95f919cb96392999c6823
Shamsul Arifin <arifinbd6@gmail.com>
2022-04-22 21:38:21 +0600

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
-# Copyright 2020 Shamsul Arifin, Crystal Technology Bangladesh Ltd.
+# Copyright 2020 Shamsul Arifin
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "ICR (Experimental)",
     "version": "1.0.0.1",
-    "author": "Crystal Technology Bangladesh",
+    "author": "Shamsul Arifin",
     "category": "Reporting",
     'summary': 'Inventory Consolidated Report.'
                'User can view consolidated report of inventory',
-    'company': 'Crystal Technology Bangladesh Ltd.',
-    'website': 'www.ctechbd.com',
     "description": """User can view consolidated report of inventory""",
     "depends": ["product", "stock"],
     "data": ["wizard/icr_report_stock_wizard.xml"],
